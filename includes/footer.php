587b5b24e1feee92896d1c0a238e94ea18e76c1f
Vishakha0121 <112022967+Vishakha0121@users.noreply.github.com>
2022-12-27 22:23:38 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,50 @@
+<style>
+    h4 {color:white;}
+    a {color:#89CFF0;}
+</style>
+<footer id="footer resources" class="p-4" style="background-color: rgba(0, 0, 0, 0.7);color:#fff">
+    <div class="container-fluid" id="inner-footer" >
+        <div class="row">
+            <div class="col-md-2 row-col">
+                <ul> 
+                    <h4>Timetable</h4>
+                    <li><a href="./timetable.php">View by Standard</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 row-col">
+                <ul>
+                    <h4>Syllabus</h4>
+                    <li><a href="./syllabus.php">View by Standard</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 row-col">
+                <ul>
+                    <h4>Explore</h4>
+                    <li><a href="../index.php">Homepage</a></li>
+                    <li><a href="#!">TEQIP III</a></li>
+                    <li><a href="#!">Training & Placements</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 row-col">
+                <ul>
+                    <h4>Social Media Links</h4>
+                    <li><a href="https://www.facebook.com/swaraj4715">Facebook</a></li>
+                    <li><a href="https://www.facebook.com/swaraj4715">Instagram</a></li>
+                    <li><a href="https://twitter.com/swaraj1729">Twitter</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 row-col">
+                <ul>
+                    <h4>Contact Us</h4>
+                    <li style="color:white"><span>School Enterprise Resource Planning Platform, Yantromitra Solutions Private Limited</span></li><br>
+                    <li><a href="https://goo.gl/maps/y3ZwTV9u2sweudjA6" target="_blank"><span>Sikkim, Ravangla, Sikkim, India - 737139</span></a></li>
+                    <li><a href="tel:+91 8002046457"><span>+91 8002046457</span></a></li>
+                    <li><a href="mailto:swarajkumarchaudhary1729@gmail.com"><span>swarajkumarchaudhary1729@gmail.com</span></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="copyright mb-3">
+        <b>&copy; Swaraj Kumar Chaudhary (2021)</b>
+    </div>
+</footer>
