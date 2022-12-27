f4e3527ff9c9da79556750ed5e11d03bb1f5cd91
Vishakha0121 <112022967+Vishakha0121@users.noreply.github.com>
2022-12-27 22:55:17 +0530

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -89,7 +89,7 @@
     <div class="container-fluid">
         <div class="row content">
             <div class="col-sm-2 sidenav hidden-xs">
-                <h4>ERP Model.</h4>
+                <h4><b>ERP Model.</b></h4>
                 <?php
                 if (isset($_GET["transport"]) && $_GET["transport"] == true) {
                     echo "
@@ -246,7 +246,53 @@
             </div>
         </div>
     </div>
-
+    <br>
+<footer id="footer resources" class="p-4" style="background-color: rgba(0, 0, 0, 0.7);color:#fff;text-align:center">
+    <div class="container-fluid" id="inner-footer" >
+        <div class="row">
+            <div class="col-md-2 row-col">
+                <ul type="none"> 
+                    <h4 style="color:white"><b>Timetable</b></h4>
+                    <li><a href="./timetable.php" style="color:#89CFF0">View by Standard</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 row-col">
+                <ul type="none">
+                    <h4 style="color:white"><b>Syllabus</b></h4>
+                    <li><a href="./syllabus.php" style="color:#89CFF0">View by Standard</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 row-col">
+                <ul type="none">
+                    <h4 style="color:white"><b>Explore</b></h4>
+                    <li><a href="../index.php" style="color:#89CFF0">Homepage</a></li>
+                    <li><a href="#!" style="color:#89CFF0">TEQIP III</a></li>
+                    <li><a href="#!" style="color:#89CFF0">Training & Placements</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 row-col">
+                <ul type="none">
+                    <h4 style="color:white"><b>Social Media Links</b></h4>
+                    <li><a href="https://www.facebook.com/swaraj4715" style="color:#89CFF0">Facebook</a></li>
+                    <li><a href="https://www.facebook.com/swaraj4715" style="color:#89CFF0">Instagram</a></li>
+                    <li><a href="https://twitter.com/swaraj1729" style="color:#89CFF0">Twitter</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 row-col">
+                <ul type="none">
+                    <h4 style="color:white"><b>Contact Us</b></h4>
+                    <li style="color:white"><span>School Enterprise Resource Planning Platform, Yantromitra Solutions Private Limited</span></li><br>
+                    <li><a href="https://goo.gl/maps/y3ZwTV9u2sweudjA6" target="_blank" style="color:#89CFF0"><span>Sikkim, Ravangla, Sikkim, India - 737139</span></a></li>
+                    <li><a href="tel:+91 8002046457" style="color:#89CFF0"><span>+91 8002046457</span></a></li>
+                    <li><a href="mailto:swarajkumarchaudhary1729@gmail.com" style="color:#89CFF0"><span>swarajkumarchaudhary1729@gmail.com</span></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="copyright mb-3">
+        <b>&copy; Swaraj Kumar Chaudhary (2021)</b>
+    </div>
+</footer>
+        <br>
 </body>
-
 </html>
